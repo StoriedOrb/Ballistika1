88e37eca486692deb30e7cd376d8bc59dc2c8adf
StoriedOrb <57195998+StoriedOrb@users.noreply.github.com>
2020-02-15 11:43:07 +0300

    Add files via upload

---
+++ Ballistika/Ballistika/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Ballistika
+{
+    partial class Ballistika
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Corner = new System.Windows.Forms.TextBox();
+            this.Speed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Height = new System.Windows.Forms.TextBox();
+            this.Lenght = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Polet = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Itog = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Polet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Time,
+            this.X,
+            this.Y});
+            this.dataGridView1.Location = new System.Drawing.Point(682, 330);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 240);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "t (сек)";
+            this.Time.Name = "Time";
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X (м)";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y (м)";
+            this.Y.Name = "Y";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Orange;
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(682, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(346, 282);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исходные данные";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.YellowGreen;
+            this.groupBox3.Controls.Add(this.Corner);
+            this.groupBox3.Controls.Add(this.Speed);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(6, 168);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(334, 108);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Параметры выстрела";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // Corner
+            // 
+            this.Corner.Location = new System.Drawing.Point(228, 28);
+            this.Corner.Name = "Corner";
+            this.Corner.Size = new System.Drawing.Size(100, 24);
+            this.Corner.TabIndex = 4;
+            // 
+            // Speed
+            // 
+            this.Speed.Location = new System.Drawing.Point(227, 72);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(100, 24);
+            this.Speed.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(7, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Угол вылета (°) ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(6, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Скорость снаряда (м/с)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.YellowGreen;
+            this.groupBox2.Controls.Add(this.Height);
+            this.groupBox2.Controls.Add(this.Lenght);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(6, 34);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 106);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Параметры орудия";
+            // 
+            // Height
+            // 
+            this.Height.Location = new System.Drawing.Point(228, 71);
+            this.Height.Name = "Height";
+            this.Height.Size = new System.Drawing.Size(100, 24);
+            this.Height.TabIndex = 3;
+            // 
+            // Lenght
+            // 
+            this.Lenght.Location = new System.Drawing.Point(228, 28);
+            this.Lenght.Name = "Lenght";
+            this.Lenght.Size = new System.Drawing.Size(100, 24);
+            this.Lenght.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(5, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Высота лафета (м)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Длина ствола (м)";
+            // 
+            // Polet
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Polet.ChartAreas.Add(chartArea1);
+            this.Polet.Location = new System.Drawing.Point(12, 46);
+            this.Polet.Name = "Polet";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Series1";
+            this.Polet.Series.Add(series1);
+            this.Polet.Size = new System.Drawing.Size(664, 487);
+            this.Polet.TabIndex = 2;
+            this.Polet.Text = "chart1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(684, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 24);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Таблица расчётов";
+            // 
+            // Itog
+            // 
+            this.Itog.Location = new System.Drawing.Point(12, 539);
+            this.Itog.Name = "Itog";
+            this.Itog.Size = new System.Drawing.Size(663, 30);
+            this.Itog.TabIndex = 4;
+            this.Itog.Text = "Провести расчёты и построить траекторию";
+            this.Itog.UseVisualStyleBackColor = true;
+            this.Itog.Click += new System.EventHandler(this.Itog_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(318, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Траектория полёта снаряда";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Ballistika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(1040, 582);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Itog);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Polet);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Ballistika";
+            this.Text = "Ballistika";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Polet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Corner;
+        private System.Windows.Forms.TextBox Speed;
+        private System.Windows.Forms.TextBox Height;
+        private System.Windows.Forms.TextBox Lenght;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Polet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Itog;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Ballistika/Ballistika/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ballistika
+{
+    public partial class Ballistika : Form
+    {
+        public Ballistika()
+        {
+            InitializeComponent();
+        }
+
+        private void Itog_Click(object sender, EventArgs e)
+        {
+            const double g = 9.81;                      //Ускорение свободного падения
+            double D, H, ug, v, ur, x0, y0, t, x, y;
+            D = Convert.ToDouble(Lenght.Text);
+            D = Convert.ToDouble(Lenght.Text);
+            D = Convert.ToDouble(Lenght.Text);
+            D = Convert.ToDouble(Lenght.Text);          //Блок ввода данных  D, H, v, ug
+            //Перевод из градусов в радианы
+            ur = ug * Math.PI / 180;
+            //Параметры начала вылета
+            x0 = 0;                 //Начало оси
+            y0 = H + D * Math.Sin(ur);      //Начальная высота 
+            t = 0;
+            y = y0;
+            while (y >= 0)          //Пока снаряд над землей
+            {
+                x = x0 + v * Math.Cos(ur) * t;
+                y = y0 + v * Math.Sin(ur) * t - g * t * t / 2;
+                //Вывод  расчетной  точки  в  таблицу  стрельбы
+                //Вывод  расчетной  точки  на  график
+                t = t + 0.1;            //Следующий момент времени
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
